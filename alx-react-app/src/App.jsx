7ff02275ca7a508b028dde7574a37574ee103d7c
Michael <themichaelsteve@gmail.com>
2024-08-12 15:46:18 +0100

    Counter added

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -7,6 +7,7 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
+import Counter from '../../alx-react-app-new/src/components/Counter'
 
 
 function App() {
@@ -23,6 +24,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <Counter />
       <WelcomeMessage />
       <Header />
       <MainContent />
