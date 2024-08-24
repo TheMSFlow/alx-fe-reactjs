35440a87425db148943088e3f4345184b3cf3077
Michael <themichaelsteve@gmail.com>
2024-08-24 13:06:13 +0100

    Task 3

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -8,6 +8,7 @@ const useRecipeStore = create(set => ({
   recommendations: [],
   
   addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
+  setRecipes: (recipes) => set({ recipes })
   deleteRecipe: (id) => set(state => ({
     recipes: state.recipes.filter(recipe => recipe.id !== id),
     filteredRecipes: state.filteredRecipes.filter(recipe => recipe.id !== id),
