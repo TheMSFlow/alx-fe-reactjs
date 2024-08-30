21647ca9f79e7ec47a69756684b8df99d86ae135
Michael <themichaelsteve@gmail.com>
2024-08-30 17:32:53 +0100

    task 1 setup

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import './App.css'
 import RegistrationForm from './components/RegistrationForm'
-import FormikForm from './components/formikForm'
+
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +10,6 @@ function App() {
   return (
     <>
       <RegistrationForm />
-      <FormikForm />
     </>
   )
 }
