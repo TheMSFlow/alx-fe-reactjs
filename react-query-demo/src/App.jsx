27ada750b4431ffe5fcdd46a3268520c91e1584e
Michael <themichaelsteve@gmail.com>
2024-08-30 17:41:55 +0100

    task 1 setup

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,5 +1,5 @@
 import { QueryClient, QueryClientProvider } from 'react-query';
-import PostsComponent from './Components/PostsComponent';
+import PostsComponent from './components/PostscComponent';
 
 const queryClient = new QueryClient();
 
