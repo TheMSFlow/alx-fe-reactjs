2a23b34c15ffcae9b1fd4aa63e6409f96829ea24
Michael <themichaelsteve@gmail.com>
2024-08-31 14:28:44 +0100

    task 2 check

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -4,7 +4,7 @@ import { Link, Outlet } from 'react-router-dom'
 const Profile = () => {
   return (
     <>
-    <div>Profile</div>
+    <div>Profile Page </div>
     <nav>
         <Link to='details'> Details </Link>
         <Link to='settings'> Settings </Link>
