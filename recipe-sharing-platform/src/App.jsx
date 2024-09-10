97fd177e93ee3196f618f9f69b8db5bbe463544c
Michael <themichaelsteve@gmail.com>
2024-09-10 13:37:13 +0100

    files

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import HomePage from './components/HomePage'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <p className='text-green-500 text-3xl'> Recipe Sharing App </p>
+    <HomePage />
     </>
   )
 }
---
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import data from '../data.json'
+
+const initialData = data
+
+const HomePage = () => {
+const [data, setData] = useState(initialData)
+
+useEffect(() => {
+    setData(initialData)
+},[])
+
+const recipeList = data.map((data) => {
+    return(
+    <ul key={data.id} className='w-96'>
+        <li>
+        <img src={data.image} width="auto" height="auto"/>
+        <h2>{data.title}</h2>
+        <p>{data.summary}</p>
+        </li>
+    </ul>
+    )
+    
+})
+  return (
+    <>
+    <h1 className='text-blue-500 text-3xl block text-center mt-4 mb-8'> Recipe Sharing App</h1>
+    <div>{recipeList}</div>
+    </>
+  )
+}
+
+export default HomePage
