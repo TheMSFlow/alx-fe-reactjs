31801f907c33c09e71360d0b98a12b159af2f556
Michael <themichaelsteve@gmail.com>
2024-09-10 20:26:46 +0100

    dynamic recipe details page

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import {BrowserRouter,Routes, Route} from 'react-router-dom'
 import HomePage from './components/HomePage'
+import RecipeDetail from './components/RecipeDetail'
 
 function App() {
 
   return (
-    <>
-    <HomePage />
-    </>
+    <BrowserRouter>
+     <Routes>
+        <Route path="/" element={<HomePage />}> Homepage </Route>
+        <Route path="/recipe/:id" element={<RecipeDetail />}> Recipe Details </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useParams, useNavigate } from 'react-router-dom';
+import Data from '../data.json'
+
+const RecipeDetail = () => {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  
+  const recipe = Data.find(recipe => recipe.id === Number(id))
+  if (!recipe) {
+    return <div> Recipe not found</div>
+  }
+
+  return (
+    <>
+      <h1>{recipe.title}</h1>
+      <img src={recipe.image} />
+      <p>{recipe.summary}</p>
+    </>
+
+  )
+}
+
+export default RecipeDetail
