afd123675ee4ceaa7f50cd5ed8de9d1ab7ed8ce3
Michael <themichaelsteve@gmail.com>
2024-09-14 12:56:03 +0100

    Responsiveness

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -11,29 +11,22 @@ useEffect(() => {
     setData(initialData)
 },[])
 
-const recipeList = data.map((data) => {
-    return(
-      <>
-        <ul key={data.id} className=' p-8 w-auto lg:inline-flex justify-start items-center '>
-          <li className='flex flex-col gap-4 p-8 bg-blue-100 shadow-md hover:shadow-2xl transition-shadow duration-200 rounded-lg lg:w-[400px]'>
-            <img src={data.image} width="auto" height="auto"/>
-            <h2 className='text-blue-950 text-3xl md:text-4xl capitalize font-medium '>{data.title}</h2>
-            <p className='text-blue-950 text-lg md:text-2xl'>{data.summary}</p>
-            <p className='text-blue-950 text-[1.15rem] md:text-[1.5rem]'><span className='font-medium'>Ingredients:</span> {data.ingredients}</p>
-            <p className='text-blue-950 text-[1.15rem] md:text-[1.5rem]'><span className='font-medium'>Preparation Steps:</span> {data.preparation}</p>
-          </li>
-        </ul>
-      </>
-    
-    )
-    
-})
   return (
     <>
     <h1 className='text-blue-950 text-3xl md:text-4xl block text-left lg:text-center mt-4  md:my-8 ml-8'> Recipe Sharing App</h1>
-    <div>{recipeList}</div>
+    <ul className=' p-8 w-auto grid gap-8 grid-cols-1 sm:grid-cols-2 xl:grid-cols-3'>
+    {data.map((data) => (
+     <li key={data.id} className='flex flex-col gap-4 p-8 bg-blue-100 shadow hover:shadow-lg transition-shadow duration-200 rounded-lg lg:w-[400px]'>
+       <img src={data.image} width="auto" height="auto"/>
+       <h2 className='text-blue-950 text-3xl md:text-4xl capitalize font-medium '>{data.title}</h2>
+       <p className='text-blue-950 text-lg md:text-2xl'>{data.summary}</p>
+       <p className='text-blue-950 text-[1.15rem] md:text-[1.5rem]'><span className='font-medium'>Ingredients:</span> {data.ingredients}</p>
+       <p className='text-blue-950 text-[1.15rem] md:text-[1.5rem]'><span className='font-medium'>Preparation Steps:</span> {data.preparation}</p>
+     </li>
+    ))}
+    </ul> 
     </>
-  )
+  );
 }
 
 export default HomePage
--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,14 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false,
-  content: [],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
-    extend: {
-      screens: {
-        'sm': '430px', // => @media (min-width: 430px) { ... }
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
