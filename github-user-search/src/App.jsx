c1bd6ee9c4003347f3e63e3b10136902a536dc98
Michael <themichaelsteve@gmail.com>
2024-09-18 13:04:51 +0100

    setup file added

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1>Hello World React!</h1>
+     <BrowserRouter>
+      <Routes>
+        <Route />
+      </Routes>
+    </BrowserRouter>
+    </>
+   
+  )
+}
+
+export default App
