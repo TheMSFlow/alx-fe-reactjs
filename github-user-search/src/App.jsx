303e9b274db496feb8abb4291e6259773fec03d8
Michael <themichaelsteve@gmail.com>
2024-09-18 13:11:55 +0100

    setup file added

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,14 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
+import Home from './components/Home'
 
 function App() {
 
   return (
     <>
-    <h1>Hello World React!</h1>
      <BrowserRouter>
       <Routes>
-        <Route />
+        <Route path='/' element={<Home />} />
       </Routes>
     </BrowserRouter>
     </>
---
+++ github-user-search/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+    <h1>Hello World React!</h1>
+    <div>Home</div>
+    </>
+  )
+}
+
+export default Home
