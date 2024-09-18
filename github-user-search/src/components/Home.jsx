d2b1517f98c321cb807b88d230d118433db0eb52
Michael <themichaelsteve@gmail.com>
2024-09-18 15:20:11 +0100

    API files

--- github-user-search/src/components/Home.jsx
+++ github-user-search/src/components/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Search from './Search'
 
 const Home = () => {
   return (
     <>
-    <h1>Hello World React!</h1>
-    <div>Home</div>
+    <main className='flex flex-col justify-start items-center w-svw h-svh'>
+        <h1 className='text-blue-900 mt-8 text-9xl mb-16'>GitHub User Search</h1>
+        <Search />
+    </main>
+    
     </>
   )
 }
