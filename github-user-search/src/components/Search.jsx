3197b3b220c1b3b04d3480d7403499eb8f1de31d
Michael <themichaelsteve@gmail.com>
2024-09-18 15:22:28 +0100

    form update

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -7,17 +7,17 @@ const Search = () => {
         setSearch(e.target.value);
     }
 
-    const handleClick = (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault();
         console.log("clicked");
     }
 
 
   return (
-    <>
+    <form>
     <input className='border-b-2 border-blue-800 text-lg py-4 px-4 lg:w-[40%] mb-6 placeholder-slate-400' onChange={handleChange} type='text'  name='search' placeholder='enter github username'/>
-    <button className='bg-blue-950 text-slate-100 text-2xl py-4 px-2 w-[200px] border rounded-2xl' onClick={handleClick}>Enter</button>
-    </>
+    <button type='submit' className='bg-blue-950 text-slate-100 text-2xl py-4 px-2 w-[200px] border rounded-2xl' onSubmit={handleSubmit}>Enter</button>
+    </form>
   )
 }
 
