019b913e92f5cc3a1109b6f508fa8c6625eda6d4
Michael <themichaelsteve@gmail.com>
2024-09-19 17:53:57 +0100

    query

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -7,9 +7,11 @@ const Search = () => {
     const [username, setUsername] = useState('');
     const [location, setLocation] = useState('');
     const [minRepos, setMinRepos] = useState('');
-    const [userData, setUserData] = useState([]);
+    const [userData, setUserData] = useState({total_count: 0, users: [] });
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(false);
+    const [page, setPage] = useState(1);
+    const [perPage] = useState(30);
 
     const handleChange = (e) => {
         console.log(e);
@@ -28,13 +30,13 @@ const Search = () => {
         setLoading("Loading...");
         setError(null);
      try{
-        const data = await fetchUserData(username, location, minRepos);
+        const data = await fetchUserData(username, location, minRepos, perPage, page);
         if (location && data.location && !data.location.toLowerCase().includes(location.toLowerCase())) {
             setError(`No users found in location: ${location}`);
-            setUserData(null);
+            setUserData({ total_count: 0, users: [] });
         } else if (minRepos && data.public_repos < parseInt(minRepos)) {
             setError(`User has less than ${minRepos} repositories`);
-            setUserData(null);
+            setUserData({ total_count: 0, users: [] });
         } else {
             setUserData(data);
         }
@@ -44,6 +46,20 @@ const Search = () => {
     } finally {
         setLoading(false);
      }
+    };
+
+    const totalPages = Math.ceil(userData.total_count / perPage); 
+
+    const handleNextPage = () => {
+        if (page < totalPages) {
+            setPage((prevPage) => prevPage + 1);
+        }
+    };
+
+    const handlePrevPage = () => {
+        if (page > 1) {
+            setPage((prevPage) => prevPage - 1);
+        }
     };
 
 
@@ -81,20 +97,26 @@ const Search = () => {
     {error && <p>{error}</p>}
     {userData && (
         <div className='w-[100%] flex flex-col justify-center items-center'>
-            <h2>Search Results: {userData.length}</h2>
+            <h2>{userData.users.length > 0 ? `Search Results: ${userData.users.length}` : null} </h2>
             <ul>
-                {userData.map((user, index) => (
-                    <li key={user.id} className='bg-slate-100 hover:bg-slate-200 p-8 rounded-3xl w-auto flex flex-col justify-center items- gap-8 shadow hover:shadow-md transition-all mt-10'>
+                {userData.users.map((user, index) => (
+                    <li key={user.id} className='bg-slate-100 hover:bg-slate-200 p-8 rounded-3xl w-auto flex flex-row justify-left items- gap-8 shadow hover:shadow-md transition-all mt-10'>
                 <img className='object-contain' src={user.avatar_url} alt={userData.name} width="100" />
                 <div>
-                    <h2 className='text-gray-700 text-6xl inline-block'>{index + 1}. {user.login}</h2>
-                    <h3 className='text-3xl mt-1'>{user.html_url}</h3>
-                    <p className='text-xl italic mt-2'>{user.bio}</p>
-                    <p>Public Repositories: {user.repos_url}</p>
+                    <h2 className='text-gray-700 text-6xl inline-block'>{index + 1 + (page - 1) * perPage}. {user.login}</h2>
+                    <h3 className='text-3xl mt-1'>Profile link: {user.html_url}</h3>
+                    <p className='text-xl italic mt-2'>Bio: {user.bio}</p>
+                    <p>Repo: {user.repos_url}</p>
                  </div> 
                 </li>
                 ))} 
             </ul>
+            {userData.users.length > 29 && <div className='flex flex-row gap-8 mt-12 mb-[10rem]'>
+                <button className='py-4 px-10 bg-gray-700 text-slate-100 rounded-lg' onClick={handlePrevPage} disabled={page === 1}>Previous</button>
+                <button className='py-4 px-10 bg-gray-700 text-slate-100 rounded-lg' onClick={handleNextPage} disabled={page.length === page.length - 1}>Next</button>
+            </div>}
+            
+                
         </div>
       )}
     </>
--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
     const BASE_URL = 'https://api.github.com/search/users?q'
 
-    export const fetchUserData = async(username,location='', minRepos = '') => {
+    export const fetchUserData = async(username,location='', minRepos = '', perPage = 30, page = 1) => {
         let query = `=${username}`;
 
         if (location) {
@@ -13,14 +13,20 @@ import axios from 'axios'
         }
 
         try{
-            const response = await axios.get(`${BASE_URL}${query}`);
-            if(response.data.items.length > 0) {
-                console.log(response.data.items);
-                return response.data.items;
-            } else {
-                throw new Error("No users found");
-            }
-             
+            const response = await axios.get(`${BASE_URL}${query}&per_page=${perPage}&page=${page}`);
+            const users = response.data.items;
+
+            const detailedUsers = await Promise.all(
+                users.map(async (user) => {
+                    const userDetailsResponse = await axios.get(user.url);
+                    return userDetailsResponse.data;  
+                })
+            );
+    
+            return {
+                total_count: response.data.total_count,
+                users: detailedUsers,
+            };
 
         } catch(err) {
         console.log("Error Msg:", err)
